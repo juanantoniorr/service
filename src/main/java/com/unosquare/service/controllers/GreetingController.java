86088c57508068bcfd13026fcf1cfda278fb0937
Juan Rosas <juan.rosas@unosquare.com>
2021-09-17 12:22:24 -0500

    Initial commit

---
+++ src/main/java/com/unosquare/service/controllers/GreetingController.java
@@ -0,0 +1,14 @@
+package com.unosquare.service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello";
+    }
+
+}
